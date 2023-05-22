525e89a59faee8d6d54fb3204b8a79a2b806e0b6
alicecha <110170331+alicecha@users.noreply.github.com>
2023-05-22 12:27:19 +0300

    Added access control

--- demo/contracts/Greeter.sol
+++ demo/contracts/Greeter.sol
@@ -1,18 +1,25 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.18;
 
 contract Greeter {
     string private greeting;
+    address private owner;
 
     constructor(string memory _greeting) {
+        owner = msg.sender;
         greeting = _greeting;
     }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    } 
 
     function greet() public view returns (string memory) {
         return greeting;
     }
 
-    function setGreeting(string memory _greeting) public {
+    function setGreeting(string memory _greeting) public onlyOwner {
         greeting = _greeting;
     }
 }
